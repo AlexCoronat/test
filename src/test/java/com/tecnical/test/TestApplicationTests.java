1b3ca9eb805245ae59bcca294187108fc6216ab1
alexc <aacoroona@gmail.com>
2025-03-29 13:11:04 -0600

    first commit

---
+++ src/test/java/com/tecnical/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnical.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
